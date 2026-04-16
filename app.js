4961c049e0cf34837e6672ca9de605c770647cce
Daniel do ProdutiveMe <143895268+produtiveme@users.noreply.github.com>
2026-04-15 21:26:19 -0300

    2.24.0

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // --- APP CORE: Funções e Configurações Compartilhadas ---
 
 // --- 1. CONFIGURAÇÃO ---
-const APP_VERSION = '2.20.0';
+const APP_VERSION = '2.24.0';
 
 // Paleta de Cores (Baseada na Identidade Visual)
 const COLORS = {
